a870ae9f3defa2417bb2cac4c7a934c0b3fa27c5
Jaime <jaime99dr@gmail.com>
2022-09-28 19:46:59 +0100

    tecnology component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { DestinationComponent } from './components/destination/destination.component';
 import { CrewComponent } from './components/crew/crew.component';
+import { TechnologyComponent } from './components/technology/technology.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'destinations', component: DestinationComponent },
   { path: 'crew', component: CrewComponent },
+  { path: 'technology', component: TechnologyComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { DestinationComponent } from './components/destination/destination.component';
 import { CrewComponent } from './components/crew/crew.component';
+import { TechnologyComponent } from './components/technology/technology.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     DestinationComponent,
-    CrewComponent
+    CrewComponent,
+    TechnologyComponent
   ],
   imports: [
     BrowserModule,
