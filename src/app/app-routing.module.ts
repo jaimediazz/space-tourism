ac6f1877939c1cd8c1f006c023ecf59f31d64b03
Jaime <jaime99dr@gmail.com>
2022-09-28 19:03:12 +0100

    crew component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { DestinationComponent } from './components/destination/destination.component';
+import { CrewComponent } from './components/crew/crew.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'destinations', component: DestinationComponent },
+  { path: 'crew', component: CrewComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { DestinationComponent } from './components/destination/destination.component';
+import { CrewComponent } from './components/crew/crew.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    DestinationComponent
+    DestinationComponent,
+    CrewComponent
   ],
   imports: [
     BrowserModule,
