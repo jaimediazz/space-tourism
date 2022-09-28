a33e0091bad7849f02d8b272208822e0377c1ca3
Jaime <jaime99dr@gmail.com>
2022-09-28 20:30:53 +0100

    technology view done

--- src/app/components/technology/technology.component.ts
+++ src/app/components/technology/technology.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import data from 'fixtures/data.json';
 
 @Component({
   selector: 'app-technology',
   templateUrl: './technology.component.html',
   styleUrls: ['./technology.component.css']
 })
-export class TechnologyComponent implements OnInit {
+export class TechnologyComponent {
+  data = data;
+  technologyIndex: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  changeTechnology(index: number) {
+    this.technologyIndex = index;
   }
 
 }
