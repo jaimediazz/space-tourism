09eeb5c37b1c6b14252899f4aaff71f7d24f9800
Jaime <jaime99dr@gmail.com>
2022-09-28 19:44:31 +0100

    crew view v1

--- src/app/components/crew/crew.component.ts
+++ src/app/components/crew/crew.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import data from 'fixtures/data.json';
 
 @Component({
   selector: 'app-crew',
   templateUrl: './crew.component.html',
   styleUrls: ['./crew.component.css']
 })
-export class CrewComponent implements OnInit {
+export class CrewComponent {
+  data = data;
+  crewIndex: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  changeCrewMember(index: number) {
+    this.crewIndex = index;
   }
-
+  
 }
